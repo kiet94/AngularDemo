9e9df48c74d6cd937af0ffeaa6e19332e374e92c
kiet94 <KIET.LQ5211@SINHVIEN.HOASEN.EDU.VN>
2019-03-04 18:16:34 +0700

    first commit

---
+++ src/environments/firebase.config.ts
@@ -0,0 +1,14 @@
+
+
+
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyALmrn1XSAigPbBTLnTJk_QsOtTNAnxWZg",
+    authDomain: "kiet-1f4e2.firebaseapp.com",
+    databaseURL: "https://kiet-1f4e2.firebaseio.com",
+    projectId: "kiet-1f4e2",
+    storageBucket: "kiet-1f4e2.appspot.com",
+    messagingSenderId: "615893671631"
+};
+
+
